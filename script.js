b0745887c57f05864d235d3495e3e6555e5dd217
Frobi <frobi.webdev@outlook.com>
2022-05-14 20:49:34 +0930

    modify-loop-to-add-sketch-functionality

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ function makeRows(rows, cols) {
     for (i = 0; i < (rows * cols); i++) {
         let cell = document.createElement("div");
         container.appendChild(cell).className = "grid-item";
+        
+        cell.addEventListener("mouseover", 
+        e => e.target.classList.add('my-color-class'));
+        // add a class to each div which when hovered, changed background-color
     };
 };
 
