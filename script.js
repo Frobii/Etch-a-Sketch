1637efa28489e37d38386128c68ea164f965b927
Frobi <frobi.webdev@outlook.com>
2022-05-19 15:14:52 +0930

    implement-rainbow-mode-functionality

--- script.js
+++ script.js
@@ -6,27 +6,38 @@ function makeRows(rows, cols) {
     for (i = 0; i < (rows * cols); i++) {
         let cell = document.createElement("div");
         container.appendChild(cell).className = "grid-item";
-        
-        cell.addEventListener("mouseover", 
-        e => e.target.classList.add('my-color-class'));
-        // add a class to each div which when hovered, changed background-color
+
+        if (rainbowButton.checked) {
+            cell.addEventListener("mouseover", 
+            () => cell.style.backgroundColor = generateRandomColor());
+        } else {
+            cell.addEventListener("mouseover", 
+            e => e.target.classList.add('my-color-class'));
+        };
+        // add a class to each div which when hovered, changes background-color
     };
 };
 
 function changeSize(size) {
     while (container.firstChild) {
         container.removeChild(container.firstChild)
-    };
+    }; // removes the previous grid
     
     size = prompt("Enter your grid size (0-100)");
     for (i = 0; i < 1; i++) 
         if (size <= 100 && size > 0) {
             return makeRows(size,size);
-        } else if (size > 100 || size < 1) {
+        } else if (size > 100) {
             size = prompt("Enter a valid grid size (0-100)");
             i--
-        };
+        } else {
+        }; // allows the close button to maintain functionality
 };
+
+const sizeButton = document.querySelector(".sizeButton");
+
+sizeButton.addEventListener("click", 
+    () => changeSize());
 
 function generateRandomColor() {
     let letters = '0123456789ABCDEF';
@@ -37,14 +48,4 @@ function generateRandomColor() {
     return color;
 };
 
-const sizeButton = document.querySelector(".sizeButton");
-
-sizeButton.addEventListener("click", 
-    () => changeSize());
-
 let rainbowButton = document.getElementById("rainbowButton");
-
-function checkboxStatus(check) {
-    check = rainbowButton.checked
-    return check;
-};
